c44fc9a55d78e54f69161c7312d787db6b066294
Karusala Anil <karusalaanil02@gmail.com>
2022-07-22 18:47:19 +0530

    added only src and pom files

---
+++ src/main/java/com/githube/cicd/GithubeCicdActionsApplication.java
@@ -0,0 +1,13 @@
+package com.githube.cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubeCicdActionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubeCicdActionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/githube/cicd/GithubeCicdActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.githube.cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubeCicdActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
