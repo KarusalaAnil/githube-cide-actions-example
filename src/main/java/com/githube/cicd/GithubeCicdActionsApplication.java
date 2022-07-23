1445299d40230375c369b1bd09dbe5508e4c1063
Karusala Anil <karusalaanil02@gmail.com>
2022-07-23 16:51:09 +0530

    added one print method in GithubeCicdActionsApplication class

--- src/main/java/com/githube/cicd/GithubeCicdActionsApplication.java
+++ src/main/java/com/githube/cicd/GithubeCicdActionsApplication.java
@@ -2,12 +2,19 @@ package com.githube.cicd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class GithubeCicdActionsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GithubeCicdActionsApplication.class, args);
 	}
 
+	@GetMapping("/welcome")
+	public String print() {
+		return "Welcome to github CI/CD project";
+	}
 }
